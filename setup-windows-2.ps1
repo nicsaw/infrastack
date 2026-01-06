c527cd6849c53dfdd78ecb5fa42543499b90f380
SAW <99191294+nicsaw@users.noreply.github.com>
2026-01-06 14:52:57 +0700

    style: rename Windows setup scripts

---
+++ setup-windows-2.ps1
@@ -0,0 +1,28 @@
+Write-Host "🔵 Tailscale" -ForegroundColor Cyan
+# https://winget.run/pkg/tailscale/tailscale
+winget install -e --id Tailscale.Tailscale --accept-source-agreements --accept-package-agreements
+Set-Service -Name Tailscale -StartupType Automatic
+# tailscale up --unattended=true
+
+Write-Host "🔵 RustDesk" -ForegroundColor Cyan
+winget install --id=RustDesk.RustDesk -e --accept-source-agreements --accept-package-agreements
+Set-Service -Name RustDesk -StartupType Automatic
+
+Write-Host "🔵 OpenSSH Server" -ForegroundColor Cyan
+# Install OpenSSH Server
+Get-WindowsCapability -Online | Where-Object Name -like "OpenSSH.Server*"
+Add-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0
+
+# Start on boot, and start now
+Set-Service -Name sshd -StartupType Automatic
+Start-Service sshd
+
+# Allow SSH from Tailscale
+New-NetFirewallRule -Name "OpenSSH-Server-Tailscale" -DisplayName "OpenSSH Server (sshd) over Tailscale" -Enabled True -Direction Inbound -Action Allow -Protocol TCP -LocalPort 22 -RemoteAddress 100.64.0.0/10
+
+Write-Host "🔵 WSL" -ForegroundColor Cyan
+wsl --update
+wsl --set-default-version 2
+schtasks /Create /F /TN "Start WSL Ubuntu-24.04" /SC ONSTART /RL HIGHEST /TR "wsl.exe -d Ubuntu-24.04 -u root --exec sleep infinity"
+wsl --install -d Ubuntu-24.04
+wsl --set-default Ubuntu-24.04
--- setup-windows.ps1
+++ setup-windows.ps1
@@ -1,28 +1,15 @@
-Write-Host "🔵 Tailscale" -ForegroundColor Cyan
-# https://winget.run/pkg/tailscale/tailscale
-winget install -e --id Tailscale.Tailscale --accept-source-agreements --accept-package-agreements
-Set-Service -Name Tailscale -StartupType Automatic
-# tailscale up --unattended=true
+Write-Host '🔵 Git' -ForegroundColor Cyan
+winget install --id Git.Git -e --source winget --accept-source-agreements --accept-package-agreements
+$env:Path = [System.Environment]::GetEnvironmentVariable('Path', 'Machine') + ';' + [System.Environment]::GetEnvironmentVariable('Path', 'User')
 
-Write-Host "🔵 RustDesk" -ForegroundColor Cyan
-winget install --id=RustDesk.RustDesk -e --accept-source-agreements --accept-package-agreements
-Set-Service -Name RustDesk -StartupType Automatic
+$repoPath = Join-Path $HOME 'pc-to-server'
+if (Test-Path $repoPath) {
+    cd $repoPath
+    git pull
+} else {
+    cd $HOME
+    git clone https://github.com/nicsaw/pc-to-server.git
+    cd $repoPath
+}
 
-Write-Host "🔵 OpenSSH Server" -ForegroundColor Cyan
-# Install OpenSSH Server
-Get-WindowsCapability -Online | Where-Object Name -like "OpenSSH.Server*"
-Add-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0
-
-# Start on boot, and start now
-Set-Service -Name sshd -StartupType Automatic
-Start-Service sshd
-
-# Allow SSH from Tailscale
-New-NetFirewallRule -Name "OpenSSH-Server-Tailscale" -DisplayName "OpenSSH Server (sshd) over Tailscale" -Enabled True -Direction Inbound -Action Allow -Protocol TCP -LocalPort 22 -RemoteAddress 100.64.0.0/10
-
-Write-Host "🔵 WSL" -ForegroundColor Cyan
-wsl --update
-wsl --set-default-version 2
-schtasks /Create /F /TN "Start WSL Ubuntu-24.04" /SC ONSTART /RL HIGHEST /TR "wsl.exe -d Ubuntu-24.04 -u root --exec sleep infinity"
-wsl --install -d Ubuntu-24.04
-wsl --set-default Ubuntu-24.04
+.\setup-windows-2.ps1
