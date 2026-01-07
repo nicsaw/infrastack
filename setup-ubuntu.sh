a32ff1d6d7b8f5bed9abff8659cb31fe7799d586
SAW <99191294+nicsaw@users.noreply.github.com>
2026-01-07 09:43:11 +0700

    feat: add NVIDIA Container Toolkit installation to Ubuntu setup script

--- setup-ubuntu.sh
+++ setup-ubuntu.sh
@@ -42,6 +42,25 @@ sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin d
 sudo usermod -aG docker "$USER"
 sudo systemctl enable --now docker
 
+# NVIDIA Container Toolkit - https://docs.nvidia.com/datacenter/cloud-native/container-toolkit/latest/install-guide.html
+echo "🔵 NVIDIA Container Toolkit"
+## Install the prerequisites for the instructions below
+sudo apt-get update && sudo apt-get install -y --no-install-recommends \
+   curl \
+   gnupg2
+
+## Configure the production repository
+curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | sudo gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg \
+  && curl -s -L https://nvidia.github.io/libnvidia-container/stable/deb/nvidia-container-toolkit.list | \
+    sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' | \
+    sudo tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
+
+## Update the packages list from the repository
+sudo apt-get update
+
+## Install the NVIDIA Container Toolkit packages
+sudo apt-get install -y nvidia-container-toolkit
+
 # Cloudflared
 echo "🔵 Cloudflared"
 cp .env.example .env
