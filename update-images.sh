a014378e139dd2693a3de1ed476cbc199a96ae6d
SAW <99191294+nicsaw@users.noreply.github.com>
2026-05-08 01:23:20 +1000

    feat: add script to update running Docker images

--- update-images.sh
+++ update-images.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 
+set -e
+
 for svc in $(docker compose ps --services --filter status=running); do
   echo "=== $svc ==="
   docker compose pull "$svc"
 done
+
+docker compose up -d
+docker image prune -f
